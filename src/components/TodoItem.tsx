ca27b53576cb28bb59f2ebd782c4b5e6e60febd4
veednak <44851565+veednak@users.noreply.github.com>
2022-04-05 02:32:19 +0300

    main

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,71 @@
+import { ItoDo } from "../components/types/data";
+import { useState, useEffect, useRef } from "react";
+import React from "react";
+
+interface ItoDoItem extends ItoDo {
+  items: ItoDo[];
+  setItems: React.Dispatch<React.SetStateAction<ItoDo[]>>;
+  //editTodo: (id: number) => void;
+
+  removeTodo: (id: number) => void;
+  inputText: (id: number, title: string) => void;
+  toggleTodo: (id: number, title: string) => void;
+}
+
+const TodoItem: React.FC<ItoDoItem> = (props) => {
+  const [value, setValue] = useState("");
+  const editTodo = (id: number) => {
+    setItems(
+      items.map((todo) => {
+        if (todo.id !== id) return todo;
+        return {
+          ...todo,
+          check: !todo.check,
+        };
+      })
+    );
+  };
+
+  const {
+    setItems,
+    items,
+    id,
+    inputText,
+    title,
+    check,
+    complete,
+    //editTodo,
+    removeTodo,
+    toggleTodo,
+  } = props;
+  return (
+    <div className="wrapper">
+      <label>
+        <input
+          className="check-box"
+          type="checkbox"
+          checked={complete}
+          onChange={() => toggleTodo(id, title)}
+        />
+
+        <input
+          className="text-list"
+          type="text-list"
+          placeholder={title}
+          value={this}
+          readOnly={check}
+          onChange={(e) => e.target.value}
+        />
+      </label>
+      <div>
+        <button className="mr-5" onClick={() => editTodo(id)}>
+          <img className="button-editing-image" />
+        </button>
+        <button onClick={() => removeTodo(id)}>
+          <img className="button-trash-image" />
+        </button>
+      </div>
+    </div>
+  );
+};
+export { TodoItem };
